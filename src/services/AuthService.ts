be4859b2eef371d4cf253552b2b49704274b1179
FireZerGG <egor.020@mail.ru>
2023-10-15 09:54:50 +0300

    authService

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,2 +1,17 @@
 import { AxiosResponse } from "axios";
 import $api from "../http";
+import { AuthResponse } from "../models/AuthResponce";
+
+export default class AuthService {
+    static async login(username: string, password: string): Promise<AxiosResponse<AuthResponse>> {
+        return $api.post<AuthResponse>('auth/login', {username, password})
+    }
+
+    static async registration(username: string, password: string): Promise<AxiosResponse<AuthResponse>> {
+        return $api.post<AuthResponse>('auth/login', {username, password})
+    }
+
+    static async logout(): Promise<void> {
+        return $api.post('auth/logout')
+    }
+}
